2ccf493eafab7bbd65291e965565dc1e191ffa80
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-04-22 17:57:45 +0200

    Nearly finished paint!

--- visual/16-04-2016/fdprPaint/fdprPaint/Program.cs
+++ visual/16-04-2016/fdprPaint/fdprPaint/Program.cs
@@ -17,6 +17,8 @@ namespace fdprPaint
             int max_x = Console.WindowWidth - 1;
             int max_y = Console.WindowHeight - 2;
             string path;
+            string prompt;
+            bool charread = false;
             char[,] mappa = new char[max_x + 1, max_y + 1];
             Console.TreatControlCAsInput = true;
 
@@ -34,16 +36,40 @@ namespace fdprPaint
                     switch (input.Key)
                     {
                         case ConsoleKey.C:
-                            matita = Convert.ToChar(readmenu("Input your char: ", 1));
+                            
+                            int left = Console.CursorLeft;
+                            int top = Console.CursorTop;
+                            while (charread == false) {
+
+                                try {
+                                    matita = Convert.ToChar(readmenu("Input your char: ", 1));
+                                    charread = true;
+                                } catch(Exception) {
+                                    charread = false;
+                                    printmenu(new string[] { "Invalid char. Please try again." }, false);
+                                    align();
+                                    System.Threading.Thread.Sleep(3000);
+                                }
+                                Console.SetCursorPosition(left, top);
+                            }
+                            charread = false;
                             break;
                         case ConsoleKey.L:
+                            
+                            prompt = "Input the file path: ";
+                            path = readmenu(prompt, 10000000);
+                            while (!readfromfile(ref mappa, path)) {
+                                prompt = "Wrong file path! Input the file path: ";
+                                path = readmenu(prompt, 10000000);
+                            };
                             break;
                         case ConsoleKey.O:
-                            path = readmenu("Input the destination file path: ", 1000000);/*
-                            do {
-                                path = readmenu("Wrong file path! Input the destination file path: ", 1000000);
-                            } while (!*/
-                            Console.WriteLine(printtofile(ref mappa, path));
+                            prompt = "Input the destination file path: ";
+                            path = readmenu(prompt, 10000000);
+                            while (!printtofile(ref mappa, path)) {
+                                prompt = "An error occurred! Input the destination file path: ";
+                                path = readmenu(prompt, 10000000);
+                            };
                             break;
                     }
 
@@ -52,40 +78,64 @@ namespace fdprPaint
                     {
                         case ConsoleKey.LeftArrow:
                             x--;
+
+                            printTo(ref x, ref y, max_x, max_y, matita, input.Key, ref mappa);
                             break;
                         case ConsoleKey.RightArrow:
                             x++;
+
+                            printTo(ref x, ref y, max_x, max_y, matita, input.Key, ref mappa);
                             break;
                         case ConsoleKey.DownArrow:
                             y++;
+
+                            printTo(ref x, ref y, max_x, max_y, matita, input.Key, ref mappa);
                             break;
                         case ConsoleKey.UpArrow:
                             y--;
+
+                            printTo(ref x, ref y, max_x, max_y, matita, input.Key, ref mappa);
                             break;
                     }
 
-                    printTo(ref x, ref y, max_x, max_y, matita, input.Key, ref mappa);
                 }
                 printmenu(new string[] { "^X: Exit", "^C Change char", "^L Load", "^O Save", "x: " + Console.CursorLeft + ", y: " + Console.CursorTop }, true);
 
             }
         }
 
+        static void align()
+        {
+
+            int left = Console.CursorLeft;
+            int top = Console.CursorTop;
+            Console.SetCursorPosition(0, 0);
+            Console.SetCursorPosition(left, top);
+        }
+
         static bool printtofile(ref char[,] mappa, string dest)
         {
 
             // x is width (oriz), y is height (vert), index 0 is x, index 1 is y
-            string result; 
+            string result = ""; 
             try
             {
-                System.IO.StreamWriter file = new System.IO.StreamWriter(@dest);
-                
-                if() {
-                    do {
-                        readmenu("File exists. Overwrite (y/n)?", 1);
-                    } while(result != "y" || result != "n")
+                if(Directory.Exists(@dest)) {
+                    printmenu(new string[] { "Path is a directory. Specify another path." }, false);
+                    align();
+                    System.Threading.Thread.Sleep(3000);
+                    return false;
+                } else if(File.Exists(@dest)) {
+                    do
+                    {
+                        result = readmenu("File exists. Overwrite (y/n)?", 1);
+                    } while (result != "y" && result != "n");
+                    if (result == "n")  { return false; } else { System.IO.File.WriteAllText (@dest, ""); };
                 }
                 
+                
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@dest);
+
                 for (int yy = 0; yy < mappa.GetLength(1); yy++)
                 {
                     for (int xx = 0; xx < mappa.GetLength(0); xx++)
@@ -107,23 +157,116 @@ namespace fdprPaint
             return true;
         }
 
+
+        static bool readfromfile(ref char[,] mappa, string dest)
+        {
+
+            // x is width (oriz), y is height (vert), index 0 is x, index 1 is y
+            Console.Clear();
+
+            try
+            {
+                if (Directory.Exists(@dest))
+                {
+                    printmenu(new string[] { "Path is a directory. Specify another path." }, false);
+                    align();
+                    System.Threading.Thread.Sleep(3000);
+                    return false;
+                }
+                else if (!File.Exists(@dest))
+                {
+                    printmenu(new string[] { "File does not exist. Specify another path." }, false);
+                    align();
+                    System.Threading.Thread.Sleep(3000);
+                    return false;
+                }
+                int width = 0;
+                int height = 0;
+                using (var reader = File.OpenText(@dest))
+                {
+                    string curline = reader.ReadLine();
+                    while (curline != null)
+                    {
+                        height++;
+                        if (curline.Length > width) width = curline.Length;
+                        curline = reader.ReadLine();
+                    }
+                }
+
+                if (height > Console.LargestWindowHeight)
+                {
+                    height = Console.LargestWindowHeight;
+                }
+                if (width > Console.LargestWindowWidth)
+                {
+                    width = Console.LargestWindowWidth;
+                }
+
+                Console.SetWindowSize(width, height);
+
+                using (var reader = File.OpenText(@dest))
+                {
+
+                    int curheight = 0;
+                    int curwidth = 0;
+                    string curline = reader.ReadLine();
+                    while (curline != null)
+                    {
+                        curheight++;
+                        if (curheight < height)
+                        {
+                            for (int x = 0; x < curline.Length; x++)
+                            {
+                                curwidth++;
+                                if (curwidth < width)
+                                {
+                                    Console.Write(curline[x]);
+                                }
+                            }
+                            Console.WriteLine();
+
+                            curwidth = 0;
+                        }
+                        curline = reader.ReadLine();
+                    }
+                }
+                Console.SetCursorPosition(0, 0);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+
+
         static string readmenu(string wut, int dove)
         {
             string input = "";
             char inputchar;
+
             int left = Console.CursorLeft;
             int top = Console.CursorTop;
+
             printmenu(new string[] { wut }, false);
 
+            align();
             for (int x = 1; x <= dove; x++)
             {
-                inputchar = Convert.ToChar(Console.Read());
+                inputchar = Console.ReadKey().KeyChar;
                 if (inputchar == '\r')
                 {
                     x = dove + 1;
-                } else input += inputchar.ToString();
+                }
+                else
+                {
+                    if(inputchar != '\r') input += inputchar.ToString();
+                }
             }
-            Console.SetCursorPosition(0, 0);
+
             Console.SetCursorPosition(left, top);
             Console.ResetColor();
             return input;
